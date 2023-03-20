88e20715665dd5088ba250ee950894ba9e546980
Schimanski04 <janata.simon04@gmail.com>
2023-03-20 17:46:22 +0100

    First commit. Basic structure of the app.

---
+++ RockPaperScissors/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RockPaperScissors;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RockPaperScissors/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RockPaperScissors;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
