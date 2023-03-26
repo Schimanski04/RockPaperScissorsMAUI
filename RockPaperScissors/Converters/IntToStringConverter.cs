062c6579aa9c4b1182c1adf12a662f7a623d5d21
Schimanski04 <janata.simon04@gmail.com>
2023-03-26 19:44:57 +0200

    For some unknown reason it works

---
+++ RockPaperScissors/Converters/IntToStringConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Converters
+{
+    class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            if (value is int)
+            {
+                if ((int)value == 1)
+                {
+                    return "Stone";
+                }
+                if ((int)value == 2)
+                {
+                    return "Paper";
+                }
+                if ((int)value == 3)
+                {
+                    return "Scissors";
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RockPaperScissors/ViewModels/MainViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RockPaperScissors.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private int _chosenValue = -1;
+        private string _textResult = string.Empty;
+        private int _pick = 0;
+
+        //For NOW ROCK == 1 ; Paper == 2 ; SCISSORS == 3
+
+        public MainViewModel()
+        {
+            RockCommand = new Command(() =>
+            {
+                ChosenValue = 1;
+                TextResult = GameResolver();
+            });
+
+            PaperCommand = new Command(() =>
+            {
+                ChosenValue = 2;
+                TextResult = GameResolver();
+            });
+
+            ScissorsCommand = new Command(() =>
+            {
+                ChosenValue = 3;
+                TextResult = GameResolver();
+            });
+        }
+
+        public int ChosenValue
+        {
+            get { return _chosenValue; }
+            set
+            {
+                _chosenValue = value;
+                OnPropertyChanged();
+                (RockCommand as Command).ChangeCanExecute();
+                (PaperCommand as Command).ChangeCanExecute();
+                (ScissorsCommand as Command).ChangeCanExecute();
+            }
+        }
+
+        public string TextResult
+        {
+            get { return _textResult; }
+            set
+            {
+                _textResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Pick
+        {
+            get { return _pick; }
+            set
+            {
+                _pick = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand RockCommand { get; private set; }
+        public ICommand PaperCommand { get; private set; }
+        public ICommand ScissorsCommand { get; private set; }
+
+
+        public string GameResolver()
+        {
+            var random = new Random();
+            Pick = random.Next(1, 4);
+
+            if (Pick == ChosenValue)
+            {
+                return "It is a tie";
+            }
+            else if ((Pick == 1 && ChosenValue == 3) || (Pick == 2 && ChosenValue == 1) || (Pick == 3 && ChosenValue == 2))
+            {
+                return "You lost";
+            }
+            else
+            {
+                return "You won";
+            }
+        }
+
+        #region MVVM
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
+    }
+}
